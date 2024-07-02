75ef9574e79d6fb6d03183a7c8c7f10108ee7eed
Ishaan Vashist <vashistishaan@gmail.com>
2024-07-02 14:20:30 +0530

    basic project

---
+++ src/WelcomeClassExample.java
@@ -0,0 +1,28 @@
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+import net.sf.saxon.exslt.Math;
+
+public class WelcomeClassExample {
+
+    public static void main(String[] args) {
+    	
+    	
+    	
+    
+    
+    
+    
+    
+}
+
+
+
+
+}
